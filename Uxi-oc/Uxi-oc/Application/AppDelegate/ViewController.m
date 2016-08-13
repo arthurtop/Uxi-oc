786481e6e5523eda57c23f7991df3c535b40ec8e
goddess <arthurtop@163.com>
2016-08-13 16:26:09 +0800

    测试了自动布局的 Masonry
    Masonry  与  button的搭配使用.

--- Uxi-oc/Uxi-oc/Application/AppDelegate/ViewController.m
+++ Uxi-oc/Uxi-oc/Application/AppDelegate/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
+#import <UIButton+LCAlignment.h>
+
 
 @interface ViewController ()
 
@@ -16,7 +19,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitle:@"facebook" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [btn setImage:[UIImage imageNamed:@"icon_facebook"] forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+    
+    
+    [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.bottom.offset(0);
+        make.right.bottom.offset(0);
+        make.top.bottom.offset(10);
+        //make.
+    }];
+    
+    
+    [btn lc_titleImageHorizontalAlignmentWithSpace:10];
+    [btn lc_titleImageVerticalAlignmentWithSpace:10];
+    //[btn lc_imageTitleVerticalAlignmentWithSpace:10];
+    //[btn lc_imageTitleHorizontalAlignmentWithSpace:10];
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
