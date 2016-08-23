5139e1fd413446e61a3e89bd968b28bb7ed8b119
goddess <arthurtop@163.com>
2016-08-23 10:20:34 +0800

    添加头文件 宏定义 8.23
    添加头文件 宏定义

---
+++ Uxi-oc/Uxi-oc/CustomClass/Util(工具类)/XLZConstHeader.h
@@ -0,0 +1,24 @@
+//
+//  XLZConstHeader.h
+//  Uxi-oc
+//
+//  Created by jecansoft on 16/8/23.
+//  Copyright © 2016年 zyhl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLZConstHeader : NSObject
+
+
+UIKIT_EXTERN NSString *const FAN_TOKEN;
+
+UIKIT_EXTERN NSString *const FAN_DATA;
+
+UIKIT_EXTERN NSString *const FAN_ERROR;
+
+UIKIT_EXTERN NSString *const FAN_RESULTCODE;
+
+
+
+@end
---
+++ Uxi-oc/Uxi-oc/CustomClass/Util(工具类)/XLZConstHeader.m
@@ -0,0 +1,23 @@
+//
+//  XLZConstHeader.m
+//  Uxi-oc
+//
+//  Created by jecansoft on 16/8/23.
+//  Copyright © 2016年 zyhl. All rights reserved.
+//
+
+#import "XLZConstHeader.h"
+
+@implementation XLZConstHeader
+
+
+
+
+
+
+
+
+
+
+
+@end
